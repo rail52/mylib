ec6e8d4ab0e007f97b3549185d951d4d97fa8df2
RAIL <railtinkoff@gmail.com>
2025-03-08 18:55:03 +0300

    + models & migrations to sqlite

--- web/models.py
+++ web/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
+
+class Book(models.Model):
+    title = models.CharField(max_length=64)
+    user = models.ForeignKey(User, on_delete=models.RESTRICT)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=64)
+    book = models.ManyToManyField(Book)
+
+
